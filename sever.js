fc7c3c120f0f7280f4efa82a3604b082c2d7fbdc
unknown <Tuantumat203@gmail.com>
2025-06-27 01:29:38 +0700

    GET Backend

--- sever.js
+++ sever.js
@@ -5,17 +5,8 @@ const app = express()
 const port = 3000;
 
 app.use(bodyParser.json())
-app.post('/n8n-webhook', (req, res) => {
-    const receivedData = req.body;
-    console.log('Dữ liệu nhận từ n8n: ', receivedData)
-    if (receivedData.status === 'success'){
-        console.log('Successfully')
-        req.status(200).send('Tin hieu thanh cong')
-    }
-    else{
-        console.error('Loi')
-        req.status(400).send('Loi')
-    }
+app.get('/n8n-webhook', (req, res) => {
+    res.status(200).send({ message: 'Data received successfully' });
 })
 app.listen(port, () => {
     console.log('Server is running')
