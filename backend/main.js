49280242c1c132fbb7dbdc65c2c7ca836bd00df4
unknown <Tuantumat203@gmail.com>
2025-06-27 00:47:51 +0700

    test

--- backend/main.js
+++ backend/main.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
   res.send('Hello, World!');
 });
 
